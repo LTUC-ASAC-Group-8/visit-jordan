fb9f6f1b3b65b7c30f5496b8aeca5cfcff73f6ce
Ahmad Abu Osbeh <ahmadabuosbeh20@gmail.com>
2021-04-19 14:11:55 +0300

    added HTML elements by js and some saving data

--- JS/discover-app.js
+++ JS/discover-app.js
@@ -1,96 +1,181 @@
+/* eslint-disable no-inner-declarations */
 'use strict';
 //run the automatic slideshow
-let slideIndex = 0;
-showSlides();
+// let slideIndex = 0;
+// showSlides();
 
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName('slides');
-  let dots = document.getElementsByClassName('dot');
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = 'none';
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) { slideIndex = 1; }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(' active', '');
-  }
-  slides[slideIndex - 1].style.display = 'block';
-  dots[slideIndex - 1].className += ' active';
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+// function showSlides() {
+//   let i;
+//   let slides = document.getElementsByClassName('slides');
+//   let dots = document.getElementsByClassName('dot');
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = 'none';
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) { slideIndex = 1; }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(' active', '');
+//   }
+//   slides[slideIndex - 1].style.display = 'block';
+//   dots[slideIndex - 1].className += ' active';
+//   setTimeout(showSlides, 2000); // Change image every 2 seconds
+// }
+
+
+//create html structure(packages) by constructor
+
+let allPackages = [];
+let bookedPackages = [];
+function Package(name, description, price, img1, img2, img3) {
+  this.name = name,
+  this.description = description,
+  this.price = price,
+  this.img1 = img1,
+  this.img2 = img2,
+  this.img3 = img3,
+
+  allPackages.push(this);
 }
 
-//run book button
-let bookButton = document.getElementById('button');
-
-// bookButton.addEventListener('click', booking);
+//create instances
+let pacakge1 = new Package('test package', 'heloo from test package', '1200$', 'https://via.placeholder.com/200x70/sss', 'https://via.placeholder.com/150x70/sss', 'https://via.placeholder.com/200x70/sss');
 
 
-//   let bokkContentHTML = document.getElementById('bokkingContent');
-//   let outputData= document.querySelectorAll('div');
-// console.log(bokkContentHTML);
-// console.log(outputData);
-//     let b = document.getElementById('bokkingContent').value
+let container = document.getElementById('parent-container');
 
-// document.getElementById('output').innerHTML = b;
+Package.prototype.renderContent = function () {
 
-// outputData.appendChild(bokkContentHTML);
 
-// for (let i = 0; i < array.length; i++) {
-//     const element = array[i];
-// }
-//store the book container in local storage
+  //main section
+  let bigSection = document.createElement('section');
+  container.appendChild(bigSection);
+  bigSection.setAttribute('class', 'first-row');
+  //booking package
+  let bookingContent = document.createElement('div');
+  bigSection.appendChild(bookingContent);
+  bookingContent.setAttribute('class', 'bookingContent');
 
-//   console.log(outputData);
-// Get form, item, and wishlist
-let addToWishList = document.getElementById('bokkingContent');
-console.log(addToWishList);
-let wishlistItem = document.querySelector('img1');
-// let page = window.open('test.html');
-// let testDiv = page.document.getElementById('output');
-// console.log(testDiv);
-// console.log(typeof page);
-// page.addEventListener('DOMContentLoaded', () => {
-//   // Now we can access the #test element on the other page
-//   const testDiv = page.document.getElementById('output');
-//   testDiv.textContent = 'Hello world!';
-//   console.log(testDiv);
-// });
-// let wishlist = document.getElementById('output');
 
-// console.log(addToWishList);
-// console.log(wishlistItem);
-// console.log(wishlist);
+  //first slide
+  let firstSlide = document.createElement('div');
+  bookingContent.appendChild(firstSlide);
+  firstSlide.setAttribute('class', 'Slides');
+  //img number
+  let imgNumber1 = document.createElement('div');
+  firstSlide.appendChild(imgNumber1);
+  imgNumber1.setAttribute('class', 'imgNumber');
+
+  //image
+  let firstImage = document.createElement('img');
+  firstSlide.appendChild(firstImage);
+  firstImage.setAttribute('class', 'package-img');
+  firstImage.setAttribute('src', this.img1);
 
 
 
-// addToWishList.addEventListener('submit', function (event) {
+  //second slide
+  let secondSlide = document.createElement('div');
+  bookingContent.appendChild(secondSlide);
+  secondSlide.setAttribute('class', 'Slides');
+  //img number
+  let imgNumber2 = document.createElement('div');
+  secondSlide.appendChild(imgNumber2);
+  imgNumber2.setAttribute('class', 'imgNumber');
 
-//   // Don't submit the form
-//   event.preventDefault();
+  //image
+  let secondImage = document.createElement('img');
+  secondSlide.appendChild(secondImage);
+  secondImage.setAttribute('class', 'package-img');
+  secondImage.setAttribute('src', this.img2);
 
-//   // Ignore it if the wishlist item is empty
-//   if (wishlistItem.length < 1) return;
-//   // img.setAttribute('src', 'images/' + this.name + '.jpeg');
-//   // Add item to wishlist
-//   wishlistItem.setAttribute('src', 'https://via.placeholder.com/200x70/sss');
 
-//   // Clear input
-//   // wishlistItem.value = '';
+  //third slide
+  let thirdSlide = document.createElement('div');
+  bookingContent.appendChild(thirdSlide);
+  thirdSlide.setAttribute('class', 'Slides');
+  //img number
+  let imgNumber3 = document.createElement('div');
+  thirdSlide.appendChild(imgNumber3);
+  imgNumber3.setAttribute('class', 'imgNumber');
 
-//   // Save the list to localStorage
-//   localStorage.setItem('wishlistItems', wishlist.innerHTML);
+  //image
+  let thirdImage = document.createElement('img');
+  thirdSlide.appendChild(thirdImage);
+  thirdImage.setAttribute('class', 'package-img');
+  thirdImage.setAttribute('src', this.img2);
 
-// }, false);
+  //add package name
+  let packageName = document.createElement('h3');
+  bookingContent.appendChild(packageName);
+  packageName.textContent = this.name;
+  packageName.setAttribute('class', 'packageName');
 
-// // Check for saved wishlist items
-// let saved = localStorage.getItem('wishlistItems');
-// console.log(saved);
+  //create booking button
+  let bookingButton = document.createElement('input');
+  bookingContent.appendChild(bookingButton);
+  bookingButton.setAttribute('class', 'bookingButton');
+  bookingButton.setAttribute('type', 'button');
+  bookingButton.setAttribute('value', 'book');
 
-// // If there are any saved items, update our list
-// if (saved) {
-//   wishlist.innerHTML = saved;
-// }
+  //create dots container
+  let dotContainer = document.createElement('div');
+  bookingContent.appendChild(dotContainer);
+  dotContainer.setAttribute('style', 'text-align:center');
+
+  //create first dot by span
+  let dotElement1 = document.createElement('span');
+  bookingContent.appendChild(dotElement1);
+  dotElement1.setAttribute('class', 'dotElement');
+
+  //create second dot by span
+  let dotElement2 = document.createElement('span');
+  bookingContent.appendChild(dotElement2);
+  dotElement2.setAttribute('class', 'dotElement');
+
+  //create third dot by span
+  let dotElement3 = document.createElement('span');
+  bookingContent.appendChild(dotElement3);
+  dotElement3.setAttribute('class', 'dotElement');
+
+
+
+  //create description content
+  let descriptionElement = document.createElement('p');
+  bookingContent.appendChild(descriptionElement);
+  descriptionElement.textContent = this.description;
+
+  let object=this;
+  //addeventlistner for book button
+  bookingButton.addEventListener('click',savingBookedPackages);
+  function savingBookedPackages() {
+
+    bookedPackages.push(object);
+    console.log(bookedPackages);
+    let bookedArrayString=JSON.stringify(bookedPackages);
+    console.log(bookedArrayString);
+    localStorage.setItem('bookedpackage',bookedArrayString);
+
+    if (bookedArrayString !==null) {
+
+    }
+  }
+
+
+
+  // if (allPackages[0] || allPackages[4] || allPackages[8]) {
+
+  // }
+  // else (allPackages[1] || allPackages[4] || allPackages[8]) {
+
+  // }
+
+};
+
+allPackages[0].renderContent();
+
+
+
+
 
 
 
