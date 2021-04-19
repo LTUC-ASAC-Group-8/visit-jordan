192d19e2058289db553cda9f29696822cccc0f1b
Yahia Qous <yahiaqous@gmail.com>
2021-04-20 00:45:47 +0300

    test video

--- JS/favourite.js
+++ JS/favourite.js
@@ -1,84 +1,112 @@
+/* eslint-disable no-inner-declarations */
+/* eslint-disable quotes */
+/* eslint-disable no-trailing-spaces */
 /* eslint-disable indent */
 'use strict';
 
-// let div = document.getElementById('imageContainer');
-// console.log(typeof div);
-
-// let stringDiv = JSON.stringify(div);
-// console.log(stringDiv);
 
 
-// let test = localStorage.setItem('div' , stringDiv);
 
-// console.log(test);
-// console.log(stringDiv);
-
-// let stringParent = localStorage.getItem('div');
-// console.log(stringParent);
-
-// let parent = JSON.parse(stringParent);
-// console.log(parent);
-
-
-let favouriteJourneys=[];
+let favouriteJourneys = [];
 function gettingItems() {
     let stringFavouriteJourneys = localStorage.getItem('favouriteJourneys');
-
-     favouriteJourneys = JSON.parse(stringFavouriteJourneys);
-
-    console.log(favouriteJourneys);
+    favouriteJourneys = JSON.parse(stringFavouriteJourneys);
+    // console.log(favouriteJourneys);
 }
 
 gettingItems();
 
 
-let imageContainer=document.getElementById("imageContainer");
-
-imageContainer.addEventListener('click',hello)
 
 
-let imageSet ,imageSet2;
+if (favouriteJourneys !== null) {
+    let imageContainer = document.getElementById('imageContainer');
 
-// console.log(favouriteJourneys);
+    function renderFavouriteImages() {
+        for (let i = 0; i < favouriteJourneys.length; i++) {
 
+            let image = document.createElement('img');
+            imageContainer.appendChild(image);
 
-// imageSet.addEventListener('click',hello);
-
-for (let i = 0; i < favouriteJourneys.length; i++) {
-    imageSet2=document.getElementById(favouriteJourneys[i].name)
-    // console.log(favouriteJourneys[i].name);
-    // console.log("hello ",imageSet);
-   
-}
-
-  
-if (imageSet2 !== null) {
-    imageSet2.addEventListener('click',hello);
-}
-
-function hello (event) {
-    imageSet=event.target;
-    console.log(imageSet)
-}
+            image.setAttribute('src', favouriteJourneys[i].image);
+            image.setAttribute('width', '200px');
+            image.setAttribute('id', favouriteJourneys[i].name);
+            // console.log(image);
+        }
+    }
+    renderFavouriteImages();
 
 
-function renderImage() {
 
-    
+
+    let imageSet;
+    console.log(favouriteJourneys);
+
     for (let i = 0; i < favouriteJourneys.length; i++) {
-
-        let image=document.createElement ('img');
-        imageContainer.appendChild(image);
-        image.setAttribute('src',favouriteJourneys[i].image);
-        image.setAttribute('width','200px');
-        image.setAttribute('id',favouriteJourneys[i].name)
-        console.log(image);
-  
+        imageSet = document.getElementById(favouriteJourneys[i].name);
+        // console.log(favouriteJourneys[i].name);
+        console.log("hello ", imageSet);
     }
 
-    
+
+    imageContainer.addEventListener('click', divClicking);
+
+    function divClicking(event) {
+        imageSet = event.target;
+        console.log('Hey ', event.target.name);
+
+
+        let parent = document.getElementById('detailsContainer');
+        let title = document.createElement('h2');
+        let video = document.createElement('video');
+        let paragraph = document.createElement('p');
+
+        parent.appendChild(title);
+        parent.appendChild(video);
+        parent.appendChild(paragraph);
+        // console.log(parent);
+
+
+        // console.log(imageSet.id);
+        // console.log(favouriteJourneys[0].name);
+        for (let i = 0; i < favouriteJourneys.length; i++) {
+            if (imageSet.id === favouriteJourneys[i].name) {
+                title.textContent=(favouriteJourneys[i].name);
+                video.setAttribute('src',favouriteJourneys[i].video);
+                paragraph.textContent=(favouriteJourneys[i].description);
+            }
+
+        }
+
+
+
+    }
+
+
+    function renderDetails() {
+        //     let parent = document.getElementById('detailsContainer');
+        //     let title = document.createElement('h2');
+        //     let video = document.createElement('video');
+        //     let paragraph = document.createElement('p');
+
+        //     parent.appendChild(title);
+        //     parent.appendChild(video);
+        //     parent.appendChild(paragraph);
+        //     // console.log(parent);
+
+
+        //     title.textContent=();
+
+        // }
+        // renderDetails();
+    }
+} else {
+    let parent = document.getElementById('link');
+    let link = document.createElement('a');
+    parent.appendChild(link);
+    link.textContent = ('Hello Empty');
+    link.setAttribute('href', 'DiscoverJordanPlaces.html');
 }
-renderImage();
 
 
 
@@ -88,25 +116,28 @@ renderImage();
 
 
 
-// var p = {
-//     "p1": "value1",
-//     "p2": "value2",
-//     "p3": "value3"
-// };
 
-// for (var key in p) {
-//     if (p.hasOwnProperty(key)) {
-//         console.log(key + " -> " + p[key]);
-//     }
+// imageContainer.addEventListener('click', hello);
+
+// let imageSet, imageSet2;
+
+// // console.log(favouriteJourneys);
+
+// // imageSet.addEventListener('click',hello);
+
+// for (let i = 0; i < favouriteJourneys.length; i++) {
+//     imageSet2 = document.getElementById(favouriteJourneys[i].name);
+//     // console.log(favouriteJourneys[i].name);
+//     // console.log("hello ",imageSet);
 // }
 
 
+// if (imageSet2 !== null) {
+//     imageSet2.addEventListener('click', hello);
+// }
 
+// function hello(event) {
+//     imageSet = event.target;
+//     console.log(imageSet);
+// }
 
-
-
-function renderResult() {
-    
- 
-    
-}
